c5466410bbaf882769878c8dacae771acb27274e
simonreynolds77 <simonreynolds77@googlemail.com>
2022-10-10 21:58:00 +0000

    quiz

--- quiz_game/cmd/quiz_game.go
+++ quiz_game/cmd/quiz_game.go
@@ -1,15 +1,58 @@
 package main
 
-import "flag"
+import (
+	"encoding/csv"
+	"flag"
+	"os"
+	"log"
+	"fmt"
+)
 
 func main(){
+
+	// println("Hello world!")
 	
 	csvfilename := flag.String(
-		"csvfile", 
-		"problems.csv", 
-		"The filename of the CSV file containing questions")
-	
+		"csvfile", // name
+		"problems.csv", // value
+		"The filename of the CSV file containing questions") // usage 	
+
 	flag.Parse()
 
-	println(*csvfilename)
+	file, err := os.Open(*csvfilename)
+	if err != nil {
+		log.Fatalf("\nError opening file:\n%s", err)
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	data, err := csvReader.ReadAll()
+	if err != nil{
+		log.Fatalf("\nError reading CSV file:\n%s", err)
+	}
+	correct_answers := 0
+	for _, value := range data {
+		question := value[0]
+		answer := value[1]
+		//question , answer := value
+		//fmt.Printf("Question: %s .... Answer: %s\n", question, answer)
+
+		fmt.Printf("Question: %s\n", question)
+
+		var input string
+		fmt.Scanln(&input)
+
+		//fmt.Printf("... you answered: %s\n", input)
+
+		if input == answer{
+			fmt.Println("Correct!")
+			correct_answers ++
+		} else {
+			fmt.Println("Incorrect!")
+		} 
+	}
+
+	fmt.Printf("All done! You have answered %d/%d questions correctly.",
+		correct_answers, len(data))
+	 //println(*csvfilename) // the * returns the value from the flag.String()
 }
